e96ebcc21236d14764ed3c129b7bf50f78ef27de
Ben Paolillo <bppao04@gmail.com>
2018-04-22 04:46:49 -0400

    Redesigned player movement to include strafe controls and to rotate the player using the mouse

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -6,16 +6,56 @@ public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform m_Target;
     [SerializeField] private float m_SmoothSpeed;
+    [SerializeField] private float m_RotationSpeed;
     [SerializeField] private Vector3 m_Offset;
+
+    private int m_EnvironmentMask;
+    private int m_MaxRayLength = 100;
 
     // Use this for initialization
     private void Start()
     {
+        m_EnvironmentMask = LayerMask.GetMask("Environment");
     }
 
     // Use LateUpdate() because it is called after the normal Update() loop
     // where the player's transform is being updated
     private void LateUpdate()
+    {
+        // Keep the cursor locked to the center of the screen
+        Cursor.lockState = CursorLockMode.Locked;
+
+        Rotate();
+        PlayerLookAtMouse();
+        FollowPlayer();
+    }
+
+    private void Rotate()
+    {
+        // Get the user's mouse input, multiply it by the rotation speed, and
+        // make it frame-independent
+        float mouseInputX = Input.GetAxis("Mouse X") * m_RotationSpeed * Time.deltaTime;
+        float mouseInputY = Input.GetAxis("Mouse Y") * m_RotationSpeed * Time.deltaTime;
+        transform.Rotate(-mouseInputY, mouseInputX, 0f);
+    }
+
+    private void PlayerLookAtMouse()
+    {
+        // Create a ray based on the current mouse position
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(mouseRay, out hit, m_MaxRayLength, m_EnvironmentMask))
+        {
+            Debug.DrawLine(mouseRay.origin, mouseRay.direction * 100, Color.red);
+            m_Target.LookAt(hit.point);
+
+            // Manually constrain the X and Z axes
+            m_Target.localEulerAngles = new Vector3(0f, transform.localEulerAngles.y, 0f);
+        }
+    }
+
+    private void FollowPlayer()
     {
         // Calculate the camera's new rotation looking down the target's forward vector
         Quaternion newRotation = Quaternion.LookRotation(m_Target.forward);
--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -5,50 +5,28 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float m_MovementSpeed;
-    [SerializeField] private float m_RotationSpeed;
-
-    private int m_EnvironmentMask;
-    private int m_MaxRayLength = 100;
 
     // Use this for initialization
     private void Start()
     {
-        m_EnvironmentMask = LayerMask.GetMask("Environment");
     }
 
     // Update is called once per frame
     private void Update()
     {
         Move();
-        //Turn();
-    }
-
-    private void LateUpdate()
-    {
-        // Manually constrain the X and Z axes
-        //transform.localEulerAngles = new Vector3(0f, transform.localEulerAngles.y, 0f);
     }
 
     private void Move()
     {
-        // Get the user input, multiply the values by the appropriate speeds, and
-        // make movement frame-independent
-        float translation = Input.GetAxis("Vertical") * m_MovementSpeed * Time.deltaTime;
-        float rotation = Input.GetAxis("Horizontal") * m_RotationSpeed * Time.deltaTime;
+        // Get the user movement input
+        float translation = Input.GetAxisRaw("Vertical");
+        float strafeTranslation = Input.GetAxisRaw("Horizontal");
 
-        transform.Translate(0f, 0f, translation);
-        transform.Rotate(0f, rotation, 0f);
-    }
-
-    private void Turn()
-    {
-        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-
-        if (Physics.Raycast(mouseRay, out hit, m_MaxRayLength, m_EnvironmentMask))
-        {
-            Debug.DrawLine(mouseRay.origin, hit.point, Color.red);
-            transform.LookAt(hit.point);
-        }
+        // Create a new translation vector, normalize it so the player does not
+        // gain an advantage when using multiple additive input keys, multiply it
+        // by the movement speed, and make it frame-independent
+        Vector3 newTranslation = new Vector3(strafeTranslation, 0f, translation);
+        transform.Translate(newTranslation.normalized * m_MovementSpeed * Time.deltaTime);
     }
 }
