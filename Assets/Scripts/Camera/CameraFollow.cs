d3f3ad605a2736ba88835db17684f77df83b5385
Ben Paolillo <bppao04@gmail.com>
2018-04-22 00:10:01 -0400

    Implemented camera following the player

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -5,23 +5,29 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform m_Target;
-    [SerializeField] private float m_Distance;
-    [SerializeField] private float m_Height;
-    [SerializeField] private float m_SmoothLag;
-    [SerializeField] private float m_MaxFollowSpeed;
-    [SerializeField] private float m_ClampHeadPosition;
-
-    private Vector3 m_HeadOffset = Vector3.zero;
-    private Vector3 m_CenterOffset = Vector3.zero;
-    private Vector3 m_Velocity = Vector3.zero;
+    [SerializeField] private float m_SmoothSpeed;
+    [SerializeField] private Vector3 m_Offset;
 
     // Use this for initialization
     private void Start()
     {
     }
 
-    // Update is called once per frame
-    private void Update()
+    // Use LateUpdate() because it is called after the normal Update() loop
+    // where the player's transform is being updated
+    private void LateUpdate()
     {
+        // Calculate the camera's new rotation looking down the target's forward vector
+        Quaternion newRotation = Quaternion.LookRotation(m_Target.forward);
+        transform.rotation = newRotation;
+
+        // Calculate the camera's new position
+        Vector3 newCameraPos = m_Target.position +
+            (newRotation * Vector3.forward * m_Offset.z + new Vector3(0f, m_Offset.y, 0f));
+
+        // Linearly interpolate between the camera's current position and its new
+        // position, smooth it out, and make it frame-independent
+        transform.position = Vector3.Lerp(
+            transform.position, newCameraPos, m_SmoothSpeed * Time.deltaTime);
     }
 }
