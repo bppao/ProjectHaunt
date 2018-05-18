84bd0c1805def3e6961b1c7732c7110545492a2c
Ben Paolillo <bppao04@gmail.com>
2018-05-17 23:07:12 -0400

    Created sword prefab and implemented weapon equipping

---
+++ Assets/Scripts/Controllers/WeaponController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    [SerializeField] private Sword m_Sword;
+
+    private Transform m_PlayerHand;
+    private BaseWeapon m_EquippedWeapon;
+
+    private void Start()
+    {
+        // Assumes the player hand transform is the first child
+        m_PlayerHand = transform.GetChild(0);
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("EquipWeapon"))
+        {
+            EquipWeapon();
+        }
+    }
+
+    private void EquipWeapon()
+    {
+        if (m_EquippedWeapon != null)
+        {
+            Debug.Log("Weapon already equipped!");
+            return;
+        }
+
+        Debug.Log("Weapon equipped!");
+        m_EquippedWeapon = Instantiate(
+            m_Sword,
+            m_PlayerHand.position,
+            m_PlayerHand.rotation,
+            m_PlayerHand);
+    }
+}
---
+++ Assets/Scripts/Weapons/BaseWeapon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseWeapon : MonoBehaviour
+{
+    [SerializeField] private float m_Damage;
+
+    protected Animator Animator;
+
+    private void Start()
+    {
+        Animator = GetComponent<Animator>();
+    }
+
+    public void PerformAttack()
+    {
+        Debug.Log("Base weapon performed attack!");
+    }
+}
