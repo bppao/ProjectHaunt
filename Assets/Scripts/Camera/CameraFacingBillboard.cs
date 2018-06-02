0567363a4de5d46b4794746ee6e088d2f879d5c9
Ben Paolillo <bppao04@gmail.com>
2018-06-02 03:29:58 -0400

    Implemented optimized / billboarded / floating enemy health bars

---
+++ Assets/Scripts/Camera/CameraFacingBillboard.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFacingBillboard : MonoBehaviour
+{
+    [SerializeField] private Camera m_Camera;
+
+    private void Update()
+    {
+        // Calculate the camera's target rotation looking down its forward vector and
+        // set the new rotation which results in a billboard effect
+        Quaternion targetRotation = Quaternion.LookRotation(m_Camera.transform.forward);
+        transform.rotation = targetRotation;
+    }
+}
--- Assets/Scripts/Enemies/BaseEnemy.cs
+++ Assets/Scripts/Enemies/BaseEnemy.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class BaseEnemy : MonoBehaviour
 {
@@ -10,6 +11,11 @@ public class BaseEnemy : MonoBehaviour
     [SerializeField] private float m_MinAttackDistance;
     [SerializeField] private float m_TimeBetweenAttacks;
     [SerializeField] private float m_Damage;
+    [SerializeField] private Canvas m_HealthBarCanvas;
+    [SerializeField] private Image m_HealthBarBackground;
+    [SerializeField] private Image m_HealthBar;
+    [SerializeField] private float m_TimeUntilHealthBarFadesOut;
+    [SerializeField] private float m_HealthBarFadeOutTime;
 
     private const string BASE_ENEMY_DEATH_ANIM_TRIGGER = "BaseEnemyDeath";
     private const float TIME_TO_CHECK_CAN_ATTACK = 0.1f;
@@ -18,14 +24,16 @@ public class BaseEnemy : MonoBehaviour
     private Rigidbody m_Rigidbody;
     private Collider m_Collider;
     private MeshRenderer m_MeshRenderer;
-    private Color m_CurrentColor;
+    private Color m_CurrentEnemyColor;
+    private Color m_HealthBarBackgroundColor;
+    private Color m_HealthBarColor;
     private BaseCharacter m_Player;
     private NavMeshAgent m_NavMeshAgent;
 
     private float m_CurrentHealth;
     public bool IsAlive { get { return m_CurrentHealth > 0f; } }
     private bool m_FadingOut;
-    private float m_AttackTimer;
+    private float m_AttackTimer, m_HealthBarFadeTimer;
 
     private void Start()
     {
@@ -33,13 +41,20 @@ public class BaseEnemy : MonoBehaviour
         m_Rigidbody = GetComponent<Rigidbody>();
         m_Collider = GetComponent<Collider>();
         m_MeshRenderer = GetComponentInChildren<MeshRenderer>();
-        m_CurrentColor = m_MeshRenderer.material.color;
+        m_CurrentEnemyColor = m_MeshRenderer.material.color;
         m_Player = GameObject.FindGameObjectWithTag("Player").GetComponent<BaseCharacter>();
         m_NavMeshAgent = GetComponent<NavMeshAgent>();
+
+        // Start with the health bar disabled
+        m_HealthBarCanvas.enabled = false;
+
+        m_HealthBarColor = m_HealthBar.color;
+        m_HealthBarBackgroundColor = m_HealthBarBackground.color;
 
         m_CurrentHealth = m_MaxHealth;
         m_FadingOut = false;
         m_AttackTimer = 0f;
+        m_HealthBarFadeTimer = 0f;
 
         StartCoroutine(CheckCanAttack());
     }
@@ -57,21 +72,13 @@ public class BaseEnemy : MonoBehaviour
             m_NavMeshAgent.enabled = false;
         }
 
-        // Update the attack timer
+        // Update timers
         m_AttackTimer += Time.deltaTime;
+        m_HealthBarFadeTimer += Time.deltaTime;
 
-        if (m_FadingOut)
-        {
-            // Fade out the enemy based on the fade out time
-            m_CurrentColor.a -= Time.deltaTime / m_FadeOutTime;
-            m_MeshRenderer.material.color = m_CurrentColor;
+        FadeOutHealthBar();
 
-            if (m_CurrentColor.a <= 0f)
-            {
-                // Destroy the enemy object when fully faded out
-                Destroy(gameObject);
-            }
-        }
+        FadeOutEnemy();
     }
 
     // Coroutine used to check if the enemy can attack the player every
@@ -103,7 +110,11 @@ public class BaseEnemy : MonoBehaviour
     public void TakeDamage(float damageAmount)
     {
         m_CurrentHealth -= damageAmount;
-        Debug.Log(name + "'s CurrentHealth: " + m_CurrentHealth);
+
+        ResetHealthBar();
+        
+        // Update the enemy's health bar
+        m_HealthBar.fillAmount = m_CurrentHealth / m_MaxHealth;
 
         if (!IsAlive)
         {
@@ -121,6 +132,9 @@ public class BaseEnemy : MonoBehaviour
         // collisions with dead enemies
         gameObject.layer = LayerMask.GetMask("Default");
 
+        // Disable the enemy's health bar canvas
+        m_HealthBarCanvas.enabled = false;
+
         // Play the enemy's death animation
         m_Animator.SetTrigger(BASE_ENEMY_DEATH_ANIM_TRIGGER);
     }
@@ -134,5 +148,57 @@ public class BaseEnemy : MonoBehaviour
     private void StartFadingOut()
     {
         m_FadingOut = true;
+    }
+
+    private void ResetHealthBar()
+    {
+        // Enable the health bar canvas
+        m_HealthBarCanvas.enabled = true;
+
+        // Reset the health bar alpha
+        m_HealthBarBackgroundColor.a = 1f;
+        m_HealthBarColor.a = 1f;
+        m_HealthBarBackground.color = m_HealthBarBackgroundColor;
+        m_HealthBar.color = m_HealthBarColor;
+
+        // Reset the fade timer
+        m_HealthBarFadeTimer = 0f;
+    }
+
+    private void FadeOutHealthBar()
+    {
+        // Check if it's time to fade out the enemy's health bar
+        if (m_HealthBarFadeTimer > m_TimeUntilHealthBarFadesOut)
+        {
+            // Fade out the enemy health bar based on the health bar fade out time
+            float alphaChangeThisFrame = Time.deltaTime / m_HealthBarFadeOutTime;
+            m_HealthBarBackgroundColor.a -= alphaChangeThisFrame;
+            m_HealthBarColor.a -= alphaChangeThisFrame;
+
+            m_HealthBarBackground.color = m_HealthBarBackgroundColor;
+            m_HealthBar.color = m_HealthBarColor;
+
+            if (m_HealthBarBackgroundColor.a <= 0f && m_HealthBarColor.a <= 0f)
+            {
+                // Disable the health bar canvas when fully faded out
+                m_HealthBarCanvas.enabled = false;
+            }
+        }
+    }
+
+    private void FadeOutEnemy()
+    {
+        if (m_FadingOut)
+        {
+            // Fade out the enemy based on the fade out time
+            m_CurrentEnemyColor.a -= Time.deltaTime / m_FadeOutTime;
+            m_MeshRenderer.material.color = m_CurrentEnemyColor;
+
+            if (m_CurrentEnemyColor.a <= 0f)
+            {
+                // Destroy the enemy object when fully faded out
+                Destroy(gameObject);
+            }
+        }
     }
 }
